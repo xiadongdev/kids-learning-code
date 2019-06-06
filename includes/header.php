586853ea992a823abc47ab0f2993f9e1e222de0f
Xia Dong <dongxiapp@gmail.com>
2019-06-06 14:42:39 -0700

    add and connect to game page

--- includes/header.php
+++ includes/header.php
@@ -3,14 +3,11 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Jekyll v3.8.5">
-    <title>Product example · Bootstrap</title>
+    <title>Kids Learning Code</title>
 
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.css" rel="stylesheet">
-    <!-- Custom styles for this template -->
+    <!-- Custom styles -->
     <link href="css/home.css" rel="stylesheet" type="text/css">
   </head>
   <body>
--- index.php
+++ index.php
@@ -9,31 +9,29 @@
     <div class="heading-banner" >
       <h1 class="display-4 font-weight-normal">Build Your Own Game</h1>
       <p class="lead font-weight-normal">Learn computer programming the fun way! Kids Learning Code is a game-based computer science program where kids type real code and see their characters react in real time.</p>
-      <a class="btn btn-outline-secondary" href="#">Coming soon</a>
+      <a class="btn btn-outline-secondary" href="#game-container">Play and Learn Now</a>
     </div>
   </div>
 </div>
 
-<div class="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3">
-<?php
-
-  $query = "SELECT * FROM games";
-  $select_all_games_query = mysqli_query($connection, $query);
-
-  while($row = mysqli_fetch_assoc($select_all_games_query)){
-?>
-  <div class="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-    <div class="my-3 p-3">
-      <h2 class="display-5"><?php echo $row['name'];?></h2>
-      <p class="lead"><?php echo $row['description'];?></p>
+<div class="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3" id="game-container">
+  <?php
+    $query = "SELECT * FROM games";
+    $select_all_games_query = mysqli_query($connection, $query);
+    while($row = mysqli_fetch_assoc($select_all_games_query)){
+  ?>
+    <div class="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+      <div class="my-3 p-3">
+        <h2 class="display-5"><a href="game.php?id=<?php echo $row['id'];?>"><?php echo $row['name'];?></a></h2>
+        <p class="lead"><?php echo $row['description'];?></p>
+      </div>
+      <div class="bg-light shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;">
+        <img class="game-screenshot" src="<?php echo $row['img_url'];?>">
+      </div>
     </div>
-    <div class="bg-light shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;">
-      <img class="game-screenshot" src="<?php echo $row['img_url'];?>">
-    </div>
-  </div>
-<?php
-  }
-?>
+  <?php
+    }
+  ?>
 </div>
 
 
